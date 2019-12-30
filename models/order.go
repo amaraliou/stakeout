d324824b87b8f776701e2aba671f1128b1f464c4
Aliou Amar <amarraliou97@gmail.com>
2019-12-30 21:29:11 +0000

    status scope for order

--- models/order.go
+++ models/order.go
@@ -1,5 +1,25 @@
 package models
 
+const (
+	OrderPending   uint8 = 0
+	OrderPayed     uint8 = 1
+	OrderReceived  uint8 = 2
+	OrderConfirmed uint8 = 3
+	OrderRefunding uint8 = 4
+	OrderRefunded  uint8 = 5
+	OrderCancel    uint8 = 6
+)
+
+var statusScope = []uint8{
+	OrderPending,
+	OrderPayed,
+	OrderReceived,
+	OrderConfirmed,
+	OrderRefunding,
+	OrderRefunded,
+	OrderCancel,
+}
+
 // Order -> Struct to hold information about a specific order from a customer
 type Order struct {
 	Base
@@ -7,5 +27,5 @@ type Order struct {
 	OrderedFrom *Shop      `json:"ordered_from"`
 	OrderItems  []*Product `json:"ordered_items"`
 	OrderTotal  float32    `json:"total_price"`
-	Status      string     `json:"status"`
+	Status      uint8      `json:"status" gorm:"type:tinyint(1)"`
 }
--- models/product.go
+++ models/product.go
@@ -3,8 +3,13 @@ package models
 // Product -> Struct to hold product information
 type Product struct {
 	Base
-	Name       string  `json:"name"`
-	BasicPrice float32 `json:"price"`
-	SoldBy     *Shop   `json:"sold_by"`
-	Reward     int     `json:"reward"`
+	Name         string  `json:"name"`
+	Description  string  `json:"description"`
+	Code         string  `json:"code"`
+	Price        float32 `json:"price"`
+	InSale       bool    `json:"is_in_sale"`
+	Discount     int     `json:"discount"`
+	DiscountUnit string  `json:"discount_unit"`
+	SoldBy       *Shop   `json:"sold_by"`
+	Reward       int     `json:"reward"`
 }
