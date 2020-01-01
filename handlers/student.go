3835b5543edd25e4eb4e5d19ab92048977379988
Aliou Amar <amarraliou97@gmail.com>
2020-01-01 16:27:09 +0000

    student handlers + config init

--- handlers/student.go
+++ handlers/student.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"net/http"
 
+	"github.com/gorilla/mux"
+
 	"github.com/amaraliou/apetitoso/models"
 	"github.com/amaraliou/apetitoso/responses"
 )
@@ -39,4 +41,41 @@ func (server *Server) CreateStudent(writer http.ResponseWriter, request *http.Re
 
 	writer.Header().Set("Location", fmt.Sprintf("%s%s/%s", request.Host, request.RequestURI, studentCreated.ID.String()))
 	responses.JSON(writer, http.StatusCreated, studentCreated)
+}
+
+// GetStudents -> handles GET /api/v1/student/
+func (server *Server) GetStudents(writer http.ResponseWriter, request *http.Request) {
+
+	student := models.Student{}
+	students, err := student.FindAllStudents(server.DB)
+	if err != nil {
+		responses.ERROR(writer, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.JSON(writer, http.StatusOK, students)
+}
+
+// GetStudentByID -> handles GET /api/v1/student/<id:uuid>
+func (server *Server) GetStudentByID(writer http.ResponseWriter, request *http.Request) {
+
+	vars := mux.Vars(request)
+	student := models.Student{}
+	studentRetrieved, err := student.FindStudentByID(server.DB, vars["id"])
+	if err != nil {
+		responses.ERROR(writer, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.JSON(writer, http.StatusOK, studentRetrieved)
+}
+
+// UpdateStudent -> handles PUT /api/v1/student/<id:uuid>
+func (server *Server) UpdateStudent(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+// DeleteStudent -> handles DELETE /api/v1/student/<id:uuid>
+func (server *Server) DeleteStudent(writer http.ResponseWriter, request *http.Request) {
+
 }
