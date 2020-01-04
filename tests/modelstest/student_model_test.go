cbce526ef291277f628266aeb27d21786cc51aa8
Aliou Amar <amarraliou97@gmail.com>
2020-01-04 13:41:43 +0000

    Finish testing Student model

--- tests/modelstest/student_model_test.go
+++ tests/modelstest/student_model_test.go
@@ -83,3 +83,63 @@ func TestGetStudentByID(t *testing.T) {
 	assert.Equal(t, foundStudent.ID, student.ID)
 	assert.Equal(t, foundStudent.Email, student.Email)
 }
+
+func TestUpdateStudent(t *testing.T) {
+
+	err := refreshStudentTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	student, err := seedOneStudent()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	studentUpdate := models.Student{
+		User: models.User{
+			Email:      "email@email.com",
+			Password:   "password",
+			IsVerified: true,
+		},
+		IsStudent:      true,
+		FirstName:      "Trump",
+		LastName:       "Bruh",
+		BirthDate:      "09/09/1956",
+		University:     "Temple University",
+		MobileNumber:   "07547775660",
+		CountryCode:    "GB",
+		GraduationYear: 2021,
+		Points:         0,
+	}
+
+	updatedStudent, err := studentUpdate.UpdateStudent(server.DB, student.ID.String())
+	if err != nil {
+		t.Errorf("This is the error updating the student: %v\n", err)
+		return
+	}
+
+	assert.Equal(t, updatedStudent.ID, studentUpdate.ID)
+	assert.Equal(t, updatedStudent.FirstName, studentUpdate.FirstName)
+}
+
+func TestDeleteStudent(t *testing.T) {
+
+	err := refreshStudentTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	student, err := seedOneStudent()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	isDeleted, err := studentInstance.DeleteStudent(server.DB, student.ID.String())
+	if err != nil {
+		t.Errorf("This is the error deleting the student: %v\n", err)
+		return
+	}
+
+	assert.Equal(t, isDeleted, int64(1))
+}
