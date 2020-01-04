c5ba9de3b2af966c805d38f33a7eb066076c5a10
Aliou Amar <amarraliou97@gmail.com>
2020-01-04 14:17:03 +0000

    Login handler base case

--- tests/handlerstest/login_handler_test.go
+++ tests/handlerstest/login_handler_test.go
@@ -1,7 +1,99 @@
 package handlerstest
 
-import "testing"
+import (
+	"bytes"
+	"errors"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"gopkg.in/go-playground/assert.v1"
+)
 
 func TestSignIn(t *testing.T) {
 
+	err := refreshStudentTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	student, err := seedOneStudent()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	samples := []struct {
+		email        string
+		password     string
+		errorMessage string
+	}{
+		{
+			email:        student.Email,
+			password:     "password",
+			errorMessage: "",
+		},
+		{
+			email:        student.Email,
+			password:     "Wrong password",
+			errorMessage: "crypto/bcrypt: hashedPassword is not the hash of the given password",
+		},
+		{
+			email:        "Wrong email",
+			password:     "password",
+			errorMessage: "record not found",
+		},
+	}
+
+	for _, v := range samples {
+
+		token, err := server.SignIn(v.email, v.password)
+		if err != nil {
+			assert.Equal(t, err, errors.New(v.errorMessage))
+		} else {
+			assert.NotEqual(t, token, "")
+		}
+	}
+}
+
+func TestLogIn(t *testing.T) {
+
+	refreshStudentTable()
+
+	_, err := seedOneStudent()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	samples := []struct {
+		inputJSON    string
+		statusCode   int
+		email        string
+		password     string
+		errorMessage string
+	}{
+		{
+			inputJSON:    `{"email": "email@email.com", "password": "password"}`,
+			statusCode:   200,
+			errorMessage: "",
+		},
+		// More cases to cover
+	}
+
+	for _, v := range samples {
+
+		req, err := http.NewRequest("POST", "/login", bytes.NewBufferString(v.inputJSON))
+		if err != nil {
+			t.Errorf("this is the error: %v", err)
+		}
+
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(server.Login)
+		handler.ServeHTTP(rr, req)
+
+		assert.Equal(t, rr.Code, v.statusCode)
+		if v.statusCode == 200 {
+			assert.NotEqual(t, rr.Body.String(), "")
+		}
+	}
 }
