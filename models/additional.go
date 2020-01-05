cf1aa357f2be601cebc85215f99cda6c7f2cbbeb
Aliou Amar <amarraliou97@gmail.com>
2020-01-05 19:51:33 +0000

    Shop and Address

--- models/additional.go
+++ models/additional.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"errors"
+
 	"github.com/jinzhu/gorm"
 )
 
@@ -15,8 +17,39 @@ type Address struct {
 	gorm.Model
 	AddressLine1 string `json:"address_1"`
 	AddressLine2 string `json:"address_2"`
-	City         string `json:"city"`
+	TownOrCity   string `json:"town_or_city"`
 	County       string `json:"county"`
 	Postcode     string `json:"postcode"` // To implement postcode validation
 	Primary      bool   `json:"is_primary_address"`
+}
+
+// Validate address -> to configure
+func (address *Address) Validate(action string) error {
+	return nil
+}
+
+// CreateAddress -> Function to create a new address
+func (address *Address) CreateAddress(db *gorm.DB) (*Address, error) {
+
+	err := db.Debug().Create(&address).Error
+	if err != nil {
+		return &Address{}, err
+	}
+
+	return address, nil
+}
+
+// GetAddressByID -> Function to retrieve an address given its ID
+func (address *Address) GetAddressByID(db *gorm.DB, id uint) (*Address, error) {
+
+	err := db.Debug().Model(Address{}).Where("id = ?", id).Take(&address).Error
+	if gorm.IsRecordNotFoundError(err) {
+		return &Address{}, errors.New("Student not found")
+	}
+
+	if err != nil {
+		return &Address{}, err
+	}
+
+	return address, nil
 }
--- models/shop.go
+++ models/shop.go
@@ -3,8 +3,11 @@ package models
 // Shop -> Struct to hold shop information (try to figure out how to handle shop reg.)
 type Shop struct {
 	Base
-	Name      string   `json:"name"`
-	AddressID uint     `json:"address_id"`
-	Address   *Address `json:"address"`
-	Admins    []*Admin `json:"admins"`
+	Name        string   `json:"name"`
+	Logo        string   `json:"logo_link"` // Amazon S3
+	Latitude    float64  `json:"latitude"`
+	Longitude   float64  `json:"longitude"`
+	Description string   `json:"description"`
+	AddressID   uint     `json:"address_id"`
+	Address     *Address `json:"address"`
 }
