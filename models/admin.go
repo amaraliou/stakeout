4a8e9a76ca0b0a9739f3c40a019c19e6ec8865cd
Aliou Amar <amarraliou97@gmail.com>
2020-01-06 11:18:52 +0000

    Admin model

--- models/admin.go
+++ models/admin.go
@@ -1,9 +1,18 @@
 package models
 
+import uuid "github.com/satori/go.uuid"
+
 // Admin -> simple admin struct
 type Admin struct {
 	Base
 	User
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Shop      Shop      `json:"shop" gorm:"foreignkey:ShopID"`
+	ShopID    uuid.UUID `json:"-"`
+}
+
+// Validate ...
+func (admin *Admin) Validate(action string) error {
+	return nil
 }
