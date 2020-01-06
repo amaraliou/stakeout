297cf3c2c10554fd36e6c20962b73145ad83646c
Aliou Amar <amarraliou97@gmail.com>
2020-01-06 10:46:28 +0000

    CreateStudent handler

---
+++ handlers/shop.go
@@ -0,0 +1,62 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/amaraliou/apetitoso/models"
+	"github.com/amaraliou/apetitoso/responses"
+)
+
+// CreateShop -> handles POST /api/v1/shop/
+func (server *Server) CreateShop(writer http.ResponseWriter, request *http.Request) {
+
+	body, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		responses.ERROR(writer, http.StatusUnprocessableEntity, err)
+	}
+
+	shop := models.Shop{}
+	err = json.Unmarshal(body, &shop)
+	if err != nil {
+		responses.ERROR(writer, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	err = shop.Validate("create")
+	if err != nil {
+		responses.ERROR(writer, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	shopCreated, err := shop.CreateShop(server.DB)
+	if err != nil {
+		responses.ERROR(writer, http.StatusInternalServerError, err)
+		return
+	}
+
+	writer.Header().Set("Location", fmt.Sprintf("%s%s/%s", request.Host, request.RequestURI, shopCreated.ID.String()))
+	responses.JSON(writer, http.StatusCreated, shopCreated)
+}
+
+// GetShops -> handles GET /api/v1/shop/
+func (server *Server) GetShops(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+// GetShop -> handles GET /api/v1/shop/<id:uuid>
+func (server *Server) GetShop(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+// UpdateShop -> handles PUT /api/v1/shop/<id:uuid>
+func (server *Server) UpdateShop(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+// DeleteShop -> handles DELETE /api/v1/shop/<id:uuid> (make sure products are deleted as well)
+func (server *Server) DeleteShop(writer http.ResponseWriter, request *http.Request) {
+
+}
