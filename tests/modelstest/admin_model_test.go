d00f119b019ef745d54cd614ba04cb3f8532582f
Aliou Amar <amarraliou97@gmail.com>
2020-01-07 19:03:24 +0000

    Setting up admin tests

---
+++ tests/modelstest/admin_model_test.go
@@ -0,0 +1,27 @@
+package modelstest
+
+import (
+	"testing"
+
+	"gopkg.in/go-playground/assert.v1"
+)
+
+func TestFindAllAdmins(t *testing.T) {
+	assert.Equal(t, 1, 1)
+}
+
+func TestCreateAdmin(t *testing.T) {
+	assert.Equal(t, 1, 1)
+}
+
+func TestFindAdminByID(t *testing.T) {
+	assert.Equal(t, 1, 1)
+}
+
+func TestUpdateAdmin(t *testing.T) {
+	assert.Equal(t, 1, 1)
+}
+
+func TestDeleteAdmin(t *testing.T) {
+	assert.Equal(t, 1, 1)
+}
