addf1024e2d57007b7e5465e6dc6ffdbcb3edf2c
Aliou Amar <amarraliou97@gmail.com>
2020-01-07 18:16:45 +0000

    Admin model functions

--- models/admin.go
+++ models/admin.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"errors"
+	"log"
+
 	"github.com/jinzhu/gorm"
 	uuid "github.com/satori/go.uuid"
 )
@@ -32,38 +35,91 @@ func (admin *Admin) BeforeSave() error {
 
 // CreateAdmin -> Function to create a new admin
 func (admin *Admin) CreateAdmin(db *gorm.DB) (*Admin, error) {
-	// To implement
-	return &Admin{}, nil
+
+	err := db.Debug().Create(&admin).Error
+	if err != nil {
+		return &Admin{}, err
+	}
+
+	return admin, nil
 }
 
 // FindAllAdmins -> Function to retrieve all admins
 func (admin *Admin) FindAllAdmins(db *gorm.DB) (*[]Admin, error) {
-	// To implement
-	return &[]Admin{}, nil
+
+	admins := []Admin{}
+	err := db.Debug().Model(&Admin{}).Limit(100).Find(&admins).Error
+	if err != nil {
+		return &[]Admin{}, err
+	}
+
+	return &admins, nil
 }
 
 // FindAllAdminsWithShopID -> Function to retrieve all admins of a given shop
 func (admin *Admin) FindAllAdminsWithShopID(db *gorm.DB, shopID string) (*[]Admin, error) {
-	// To implement
-	return &[]Admin{}, nil
+
+	admins := []Admin{}
+	err := db.Debug().Model(&Admin{}).Where("shop_id = ?", shopID).Find(&admins).Error
+	if err != nil {
+		return &[]Admin{}, err
+	}
+
+	return &admins, nil
 }
 
-// GetAdminByID -> Function to retrieve an admin given its ID
-func (admin *Admin) GetAdminByID(db *gorm.DB, id string) (*Admin, error) {
-	// To implement
-	return &Admin{}, nil
+// FindAdminByID -> Function to retrieve an admin given its ID
+func (admin *Admin) FindAdminByID(db *gorm.DB, id string) (*Admin, error) {
+
+	err := db.Debug().Model(Admin{}).Where("id = ?", id).Take(&admin).Error
+	if err != nil {
+		return &Admin{}, err
+	}
+
+	return admin, nil
 }
 
 // UpdateAdmin -> Function to update a given admin
 func (admin *Admin) UpdateAdmin(db *gorm.DB, id string) (*Admin, error) {
-	// To implement
-	return &Admin{}, nil
+
+	err := admin.BeforeSave()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Debug().Model(Admin{}).Updates(&admin).Error
+	if err != nil {
+		return &Admin{}, err
+	}
+
+	return admin.FindAdminByID(db, id)
 }
 
 // DeleteAdmin -> Function to delete an admin
 func (admin *Admin) DeleteAdmin(db *gorm.DB, id string) (int64, error) {
-	// To implement
-	// Case 1: Delete admin but there are other remaining admins for shop
-	// Case 2: Delete admin and there are no more admins for shop -> delete shop
-	return 0, nil
+
+	shopID := admin.ShopID.String()
+	shop := Shop{}
+	originalDB := db
+	db = db.Debug().Model(&Admin{}).Where("id = ?", id).Take(&Admin{}).Delete(&Admin{})
+	if db.Error != nil {
+		return 0, db.Error
+	}
+
+	remainingAdmins, err := admin.FindAllAdminsWithShopID(db, shopID)
+	if err != nil {
+		return db.RowsAffected, errors.New("Couldn't retrieve remaining admins")
+	}
+
+	if len(*remainingAdmins) == 0 {
+
+		shopRowsAffected, err := shop.DeleteShop(originalDB, shopID)
+		if err != nil {
+			return db.RowsAffected, errors.New("Couldn't delete the associated shop")
+		}
+
+		return db.RowsAffected + shopRowsAffected, nil
+	}
+
+	return db.RowsAffected, nil
 }
