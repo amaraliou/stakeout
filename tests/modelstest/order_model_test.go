54aee78e3751674135f0b54ad13ebe342159bc92
Aliou Amar <amarraliou97@gmail.com>
2020-01-30 12:25:44 +0000

    Test FindAll functions for orders

--- tests/modelstest/order_model_test.go
+++ tests/modelstest/order_model_test.go
@@ -7,6 +7,72 @@ import (
 	"gopkg.in/go-playground/assert.v1"
 )
 
+func TestFindAllOrders(t *testing.T) {
+
+	err := refreshEverything()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = seedOrders()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	orders, err := orderInstance.FindAllOrders(server.DB)
+	if err != nil {
+		t.Errorf("This is the error getting the orders: %v\n", err)
+		return
+	}
+
+	assert.Equal(t, len(*orders), 2)
+}
+
+func TestFindOrdersByShop(t *testing.T) {
+
+	err := refreshEverything()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	orders, err := seedOrders()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ordersByShop, err := orderInstance.FindAllOrdersByShop(server.DB, orders[0].ShopID.String())
+	if err != nil {
+		t.Errorf("This is the error getting the orders by shop: %v\n", err)
+		return
+	}
+
+	assert.Equal(t, len(*ordersByShop), 2)
+}
+
+func TestFindAllOrdersByStudent(t *testing.T) {
+
+	err := refreshEverything()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	orders, err := seedOrders()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ordersByStudent, err := orderInstance.FindAllOrdersByStudent(server.DB, orders[0].UserID.String())
+	if err != nil {
+		t.Errorf("This is the error getting the orders by student: %v\n", err)
+		return
+	}
+
+	testOrders := *ordersByStudent
+
+	assert.Equal(t, len(*ordersByStudent), 2)
+	assert.Equal(t, orders[0].ShopID, testOrders[0].ShopID)
+}
+
 func TestFindOrderByID(t *testing.T) {
 
 	err := refreshEverything()
